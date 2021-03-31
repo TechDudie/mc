7cc0555b9952e5d1664bd1535b379434803f1137
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 09:41:15 -0500

    Create mapgen.lua

---
+++ mc_sweet_berry/mapgen.lua
@@ -0,0 +1,10 @@
+minetest.register_decoration({
+      deco_type = "simple",
+      place_on = {"mcl_core:dirt_with_grass"},
+      sidelen = 16,
+      fill_ratio = 0.1,
+      biomes = {"Taiga","Forest"},
+      y_max = mcl_vars.mg_overworld_max,
+      y_min = mcl_vars.mg_overworld_min,
+      decoration = "mc:sweet_berry_bush_2"
+})
