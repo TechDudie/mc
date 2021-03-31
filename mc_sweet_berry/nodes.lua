cf29663f13721f8a842596b187085029e20195af
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 09:57:04 -0500

    Update nodes.lua

--- mc_sweet_berry/nodes.lua
+++ mc_sweet_berry/nodes.lua
@@ -1,22 +1,22 @@
-minetest.register_node("mc:sweet_berry_bush_0", {
+minetest.register_node("mc_sweet_berry:sweet_berry_bush_0", {
      drawtype = "plantlike",
      tiles = {"sweet_berry_bush_0.png"},
      damage_per_second = 1,
      drop = ""
  })
- minetest.register_node("mc:sweet_berry_bush_1", {
+ minetest.register_node("mc_sweet_berry:sweet_berry_bush_1", {
      drawtype = "plantlike",
      tiles = {"sweet_berry_bush_1.png"},
      damage_per_second = 1,
      drop = ""
  })
- minetest.register_node("mc:sweet_berry_bush_2", {
+ minetest.register_node("mc_sweet_berry:sweet_berry_bush_2", {
      drawtype = "plantlike",
      tiles = {"sweet_berry_bush_2.png"},
      damage_per_second = 2,
      drop = "mc:sweet_berry 2"
  })
- minetest.register_node("mc:sweet_berry_bush_3", {
+ minetest.register_node("mc_sweet_berry:sweet_berry_bush_3", {
      drawtype = "plantlike",
      tiles = {"sweet_berry_bush_3.png"},
      damage_per_second = 2,
