149e440a809dc83ecee5760392e98f09dfd0c364
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 09:43:51 -0500

    Create nodes.lua

---
+++ mc_sweet_berry/nodes.lua
@@ -0,0 +1,24 @@
+minetest.register_node("mc:sweet_berry_bush_0", {
+     drawtype = "plantlike",
+     tiles = {"sweet_berry_bush_0.png"},
+     damage_per_second = 1,
+     drop = ""
+ })
+ minetest.register_node("mc:sweet_berry_bush_1", {
+     drawtype = "plantlike",
+     tiles = {"sweet_berry_bush_1.png"},
+     damage_per_second = 1,
+     drop = ""
+ })
+ minetest.register_node("mc:sweet_berry_bush_2", {
+     drawtype = "plantlike",
+     tiles = {"sweet_berry_bush_2.png"},
+     damage_per_second = 2,
+     drop = "mc:sweet_berry 2"
+ })
+ minetest.register_node("mc:sweet_berry_bush_3", {
+     drawtype = "plantlike",
+     tiles = {"sweet_berry_bush_3.png"},
+     damage_per_second = 2,
+     drop = "mc:sweet_berry 3"
+ })
