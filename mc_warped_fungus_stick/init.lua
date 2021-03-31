36ab22275336fbdeaf41ceffb5875e15242c640a
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 09:55:13 -0500

    Update init.lua

--- mc_warped_fungus_stick/init.lua
+++ mc_warped_fungus_stick/init.lua
@@ -1,9 +1,9 @@
-minetest.register_craftitem("mc:warped_fungus_stick", {
+minetest.register_craftitem("mc_warped_fungus_stick:warped_fungus_stick", {
     description = "Warped Fungus on a Stick",
     inventory_image = "warped_fungus_stick.png"
 })
 minetest.register_craft({
     type = "shapeless",
-    output = "mc:warped_fungus_stick",
+    output = "mc_warped_fungus_stick:warped_fungus_stick",
     recipe = {"mcl_fishing:fishing_rod", "mcl_mushroom:warped_fungus"}
 })
