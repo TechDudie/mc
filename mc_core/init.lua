2e6da582fdf3a10648a88bf0168164c98e932e66
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 10:01:02 -0500

    Update init.lua

--- mc_core/init.lua
+++ mc_core/init.lua
@@ -1,4 +1,1 @@
-dofile(minetest.get_modpath("mc_sweet_berry") .. "/items.lua")
-dofile(minetest.get_modpath("mc_sweet_berry") .. "/nodes.lua")
-dofile(minetest.get_modpath("mc_sweet_berry") .. "/mapgen.lua")
-dofile(minetest.get_modpath("mc_sweet_berry") .. "/abm.lua")
+
