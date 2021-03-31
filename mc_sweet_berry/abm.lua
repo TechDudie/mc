fbcedd0001d5c7678c427c8fca36045b9eed356a
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 10:02:12 -0500

    Update abm.lua

--- mc_sweet_berry/abm.lua
+++ mc_sweet_berry/abm.lua
@@ -1,18 +1,18 @@
 minetest.register_abm({
-     nodenames = {"mc_sweet_berry:sweet_berry_bush_0"},
-     interval = 10.0,
-     chance = 16,
-     action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos, {name = "mc:sweet_berry_bush_1"}) end
- })
- minetest.register_abm({
-     nodenames = {"mc_sweet_berry:sweet_berry_bush_1"},
-     interval = 10.0,
-     chance = 16,
-     action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos, {name = "mc:sweet_berry_bush_2"}) end
- })
- minetest.register_abm({
-     nodenames = {"mc_sweet_berry:sweet_berry_bush_2"},
-     interval = 10.0,
-     chance = 16,
-     action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos, {name = "mc:sweet_berry_bush_3"}) end
- })
+    nodenames = {"mc_sweet_berry:sweet_berry_bush_0"},
+    interval = 10.0,
+    chance = 16,
+    action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos, {name = "mc:sweet_berry_bush_1"}) end
+})
+minetest.register_abm({
+    nodenames = {"mc_sweet_berry:sweet_berry_bush_1"},
+    interval = 10.0,
+    chance = 16,
+    action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos, {name = "mc:sweet_berry_bush_2"}) end
+})
+minetest.register_abm({
+    nodenames = {"mc_sweet_berry:sweet_berry_bush_2"},
+    interval = 10.0,
+    chance = 16,
+    action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos, {name = "mc:sweet_berry_bush_3"}) end
+})
