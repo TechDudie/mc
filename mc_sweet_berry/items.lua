302ddc8d9d04367f68ac8a76c857b5d58d4140f8
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-03-31 09:55:36 -0500

    Update items.lua

--- mc_sweet_berry/items.lua
+++ mc_sweet_berry/items.lua
@@ -1,4 +1,4 @@
-minetest.register_craftitem("mc:sweet_berry", {
+minetest.register_craftitem("mc_sweet_berry:sweet_berry", {
      description = "Sweet Berry",
      inventory_image = "sweet_berry.png",
      on_use = minetest.item_eat(2)
