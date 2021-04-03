8db2ec4215dbc7095480a59fea2b7ea6d6eb6d2a
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-03 09:20:59 -0500

    Update init.lua

--- mc_bamboo/init.lua
+++ mc_bamboo/init.lua
@@ -1,1 +1,22 @@
-
+minetest.register_craftitem("mc_bamboo:bamboo_item", {
+    description = "Bambooo",
+    inventory_image = "item.png"
+})
+minetest.register_node("mc_bamboo:bamboo", {
+    description = "Bamboo Node",
+    tiles = {
+        "bamboo_top.png",
+        "bamboo_top.png",
+        "bamboo.png",
+        "bamboo.png",
+        "bamboo.png",
+        "bamboo.png"
+    },
+    drop = "mc_bamboo:bamboo_item"
+})
+minetest.register_abm({
+    nodenames = {"mc_bamboo:bamboo"},
+    interval = 10.0,
+    chance = 4,
+    action = function(pos, node, active_object_count, active_object_count_wider) minetest.set_node(pos[y] = pos.y + 1, {name = "mc_bamboo:bamboo"}) end
+})
