8aefdfdee69860e2bb8685d95ec17c06780b7ac4
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-04 09:47:20 -0500

    Refix commit fbb39ca fixing issue #4

--- mc_bamboo/init.lua
+++ mc_bamboo/init.lua
@@ -19,7 +19,7 @@ minetest.register_abm({
     interval = 10.0,
     chance = 4,
     action = function(pos, node, active_object_count, active_object_count_wider)
-        grow_pos = {pos.x, pos,y + 1, pos,z}
+        grow_pos = {pos.x, pos.y + 1, pos.z}
         minetest.set_node(grow_pos, {name = "mc_bamboo:bamboo"})
     end
 })
