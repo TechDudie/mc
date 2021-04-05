0f7c2d5c487663d3312c187059b24511b134447c
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-05 12:18:36 -0500

    Update init.lua
    #5

--- mc_conduits/init.lua
+++ mc_conduits/init.lua
@@ -1,1 +1,8 @@
-
+minetest.register_node("mc:conduit", {
+    description = "Conduit",
+    tiles = {"conduit.png"}
+})
+minetest.register_node("mc:conduit_active", {
+    description = "Active Conduit",
+    tiles = {"conduit.png"}
+})
