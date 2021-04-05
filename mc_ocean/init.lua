a683dcafc191408c7540f27e0f26d1442edfab23
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-05 15:09:28 -0500

    Update init.lua
    #5

--- mc_ocean/init.lua
+++ mc_ocean/init.lua
@@ -1,1 +1,8 @@
-
+minetest.register_craftitem("mc_ocean:heart", {
+    description = "Heart of the Sea",
+    inventory_image = "heart.png"
+})
+minetest.register_craftitem("mc_ocean:shell", {
+    description = "Nautilus Shell",
+    inventory_image = "shell.png"
+})
