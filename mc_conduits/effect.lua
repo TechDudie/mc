e16ee47c321b5ef347114bb0d317e5e9277d6d2d
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-05 15:05:34 -0500

    Update effect.lua
    #5

--- mc_conduits/effect.lua
+++ mc_conduits/effect.lua
@@ -1,12 +1,12 @@
 function effect(pos)
-	  local players = minetest.get_objects_inside_radius(pos, 96)
+	local players = minetest.get_objects_inside_radius(pos, 96)
   	if next(players) == nil then return false end
   	for _, player in pairs(players) do
-    		local name = player:get_player_name()
-    		if name ~= "" then
+    	local name = player:get_player_name()
+    	if name ~= "" then
             mcl_potions.swiftness_func(name, 4, 4)
-  		    	mcl_potions.water_breathing_func(name, nil, 4)
+            mcl_potions.water_breathing_func(name, nil, 4)
             mcl_potions.night_vision_func(name, nil, 4)
-	    	end
+	    end
   	end
 end
