2052b28143fd40c3f5f1491c737189f226f30111
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-07 19:39:12 -0500

    Update init.lua
    #13

--- mc_netherite/init.lua
+++ mc_netherite/init.lua
@@ -1,1 +1,16 @@
-
+minetest.register_craftitem("mc_netherite:ingot", {
+    description = "Netherite Ingot",
+    inventory_image = "ingot.png"
+})
+minetest.register_craftitem("mc_netherite:scrap", {
+    description = "Netherite Scrap",
+    inventory_image = "scrap.png"
+})
+minetest.register_node("mc_netherite:block", {
+    description = "Netherite Block",
+    tiles = {"block.png"}
+})
+minetest.register_node("mc_netherite:debris", {
+    description = "Ancient Debris",
+    tiles = {"debris.png"}
+})
